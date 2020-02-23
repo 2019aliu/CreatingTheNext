44f56fe95272a2d18003630d3d261da63eb555ff
2019aliu <aliu338@gatech.edu>
2020-02-23 12:37:13 -0500

    add data, basic website pt. 1

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import db
 from flask import Flask, redirect, url_for, render_template, request, send_file, make_response
 from socio_visual import do_plot
-from io import StringIO
-import
+import io
+import base64
+import matplotlib.pyplot as plt
+import urllib.parse
+import pandas as pd
 app = Flask(__name__)
-
+PORT = 8000
 
 @app.route('/')
 def index():
-    img = StringIO()
-
-    plt.plot()
-    return render_template('index.html')
+    bytes_obj = do_plot()
+    plot_url = urllib.parse.quote(base64.b64encode(bytes_obj.read()).decode())
+    return render_template('index.html', plot_url=plot_url)
 
 
 @app.route('/unemployment')
@@ -46,8 +48,24 @@ def glob_unemp():
     return render_template('global-unemployment.html')
 
 
+def do_plot():
+    # img = StringIO()
+    bytes_image = io.BytesIO()
+    y = [1, 2, 3, 4, 5]
+    x = [0, 2, 1, 3, 4]
+
+    plt.plot(x, y)
+    plt.savefig(bytes_image, format='png')
+    plt.close()
+    bytes_image.seek(0)
+    return bytes_image
+
+    # unemploymentCollection = db.getCollection("unemployment_rate")
+    # df_unrate = pd.DataFrame(list(unemploymentCollection.find())
+    # plt.plot()
+
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(port=PORT)
 
 
