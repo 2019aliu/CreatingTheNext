893dcf91c71f7c3f52e920f76c51362ebc96782e
2019aliu <aliu338@gatech.edu>
2020-02-23 07:38:38 -0500

    Basic structure for website, also line graphs not scatter?

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 import db
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
+
 @app.route('/')
-def flask_mongodb_atlas():
-    return "flask mongodb atlas!"
+def index():
+    return render_template('index.html')
+
+
+@app.route('/unemployment')
+def unemployment():
+    return render_template('unemployment.html')
+
+
+@app.route('/homelessness')
+def homeless():
+    return render_template('homeless.html')
+
+
+@app.route('/gdp')
+def gdp():
+    return render_template('gdp.html')
 
 @app.route("/test")
 def test():
